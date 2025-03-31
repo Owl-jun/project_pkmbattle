396b4ef21f209dbe8fb702519e90f19514c96204
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-03-31 10:52:19 +0900

    로그인씬 배경화면 안나오는 문제 해결

--- Client/LoginScene.hpp
+++ Client/LoginScene.hpp
@@ -17,8 +17,7 @@
 
 class LoginScene : public BaseScene {
 private:
-    // - 화면에 표시할 것들임 -
-    // 이미지 -> sf::Texture(경로) , std::optional<sf::Sprite>
+
     sf::Texture bgTex;
     std::optional<sf::Sprite> bg;
     sf::Vector2f bgtextureSize;
@@ -40,10 +39,10 @@ public:
     }
 
     void init() override {
-        bgtextureSize = static_cast<sf::Vector2f>(bgTex.getSize());
-        windowSize = static_cast<sf::Vector2f>(GameManager::getInstance().getWindow().getSize());
         // 이미지 생성 예시 , 리소스매니저에서 받고 , emplace 
         bgTex = ResourceManager::getInstance().getTexture("C:/Source/project_pkmbattle/Client/assets/introbg.png");
+        bgtextureSize = static_cast<sf::Vector2f>(bgTex.getSize());
+        windowSize = static_cast<sf::Vector2f>(GameManager::getInstance().getWindow().getSize());
         bg.emplace(bgTex);
         bg->setScale({ (windowSize.x / bgtextureSize.x), (windowSize.y / bgtextureSize.y) });
         bg->setPosition({ 0.f, 0.f });
